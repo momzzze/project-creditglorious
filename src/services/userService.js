838bd0e6baf3505ef72fb652e2e67c03f29cbfc4
nikola.ninov <momzzze@abv.bg>
2024-02-05 19:29:58 +0200

    add service to get social accounts

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,12 +3,28 @@ import axios from "axios";
 
 const getUserBySearchingQuote = async (searchingQuote) => {
     try {
+        console.log(searchingQuote);
         const response = await axios.get(`https://api.github.com/users/${searchingQuote}`);
         if (response.status === 404) {
             throw new Error("User not found");
-        }else if(response.status === 200){
+        } else if (response.status === 200) {
             return response.data;
-        }else{
+        } else {
+            throw new Error("Something went wrong");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getUserSocialAccounts = async (username) => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${username}/social_accounts`);
+        if (response.status === 404) {
+            throw new Error("User not found");
+        } else if (response.status === 200) {
+            return response.data;
+        } else {
             throw new Error("Something went wrong");
         }
     } catch (error) {
@@ -16,5 +32,4 @@ const getUserBySearchingQuote = async (searchingQuote) => {
     }
 }
 
-
-export { getUserBySearchingQuote }
+export { getUserBySearchingQuote, getUserSocialAccounts }
