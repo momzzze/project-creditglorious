fc38461675544ad4e282ca2c590835110194e2b3
nikola.ninov <momzzze@abv.bg>
2024-02-06 10:20:13 +0200

    add dark/light mode colors

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { getUserBySearchingQuote } from "../services/userService"
 import toast from "react-hot-toast";
@@ -21,13 +22,13 @@ const SearchForm = ({ onSearch }) => {
             <input
                 type="text"
                 placeholder="Search a GitHub User"
-                className="border w-1/3 border-gray-300 p-2 rounded-l-lg"
+                className="border w-1/3 border-slate-300 p-2 rounded-l-lg dark:bg-slate-500 dark:border-white dark:text-white hover:bg-gray-100  dark:placeholder:text-white focus:outline-none"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
             />
             <button
                 type="submit"
-                className="border-t border-r border-b w-20 border-gray-300 p-2 rounded-r-lg hover:bg-gray-100 "
+                className="border-t border-r border-b w-20 border-gray-300 p-2 rounded-r-lg hover:bg-gray-100 dark:bg-slate-700 dark:text-white"
             >
                 Search
             </button>
--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+/* eslint-disable react/prop-types */
+// since we do not use typescript, we can ignore the prop-types rule
 import { FaTwitter, FaLinkedin, FaYoutube, FaShare } from 'react-icons/fa';
-import { getUserSocialAccounts } from '../services/userService';
 
 const SocialMediaIcons = ({ account }) => {
     const { provider, url } = account;
@@ -9,12 +9,12 @@ const SocialMediaIcons = ({ account }) => {
         window.open(url, '_blank');
     };
     return (
-        <div className={`flex flex-row  space-x-3 justify-center items-center border ${provider === 'youtube' ? "border-red-700" : "border-blue-700"} px-3`}>
-            {provider === 'twitter' && <FaTwitter className='text-blue-500 mr-2 md:flex'/>}
-            {provider === 'youtube' && <FaYoutube className='text-red-500 mr-2' />}
-            {provider === 'linkedin' && <FaLinkedin className='text-blue-500' />}
+        <div className={`flex flex-row  space-x-3 justify-center items-center border ${provider === 'youtube' ? "border-red-700 dark:border-red-400" : "text-blue-700 dark:text-blue-300"} px-3`}>
+            {provider === 'twitter' && <FaTwitter className='text-blue-500 dark:text-blue-300 mr-2 md:flex' />}
+            {provider === 'youtube' && <FaYoutube className='text-red-500 dark:text-red-400 mr-2' />}
+            {provider === 'linkedin' && <FaLinkedin className='text-blue-500 dark:text-blue-300' />}
             {provider !== 'twitter' && provider !== 'youtube' && provider !== 'linkedin' && <FaShare className='text-blue-500' />}
-            <button className={`${provider === 'youtube' ? "text-red-700" : "text-blue-700"}`} onClick={handleClick}>{provider === 'generic' ? 'Other' : provider}</button>
+            <button className={`${provider === 'youtube' ? "text-red-700 dark:text-red-400" : "text-blue-700 dark:text-blue-300"}`} onClick={handleClick}>{provider === 'generic' ? 'Other' : provider}</button>
         </div>
     )
 }
